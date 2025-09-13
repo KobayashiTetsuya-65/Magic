b208cd7f6f5144d56c647462ad8c637e53587648
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-09-13 13:09:34 +0900

    [update]カメラの基準をターゲットの位置からカメラの正面方向へ

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -10,10 +10,10 @@ public class PlayerLook : MonoBehaviour
     [Header("標的"), SerializeField] private List<GameObject> _points = new List<GameObject>();
     [SerializeField] private float _cameraSpeed = 2f;
     private List<Vector3> _trPointsList = new List<Vector3>();
-    private Vector3 _trTarget,_direction;
-    private Transform _trPlayer, _trCamera;
+    private Vector3 _trTarget,_direction,_toCandidate;
+    private Transform _trPlayer, _trCamera,_bestTarget,_candidate;
     private Quaternion _rotation;
-    private float _yRotate,_baseYAngle,_currentYAngle;
+    private float _yRotate,_baseYAngle,_currentYAngle,_bestAngle,_dot,_angle;
     public bool Focus = false;
     private int _targetIndex;
     // Start is called before the first frame update
@@ -37,8 +37,9 @@ public class PlayerLook : MonoBehaviour
                 _yRotate = 0;
             }
         }
-        if (Input.GetKeyDown(KeyCode.L) && _targetIndex < _points.Count - 1) _targetIndex++;
-        if (Input.GetKeyDown(KeyCode.J) && _targetIndex > 0) _targetIndex--;
+        if (Input.GetKeyDown(KeyCode.L)) SwitchTarget(true);
+        if (Input.GetKeyDown(KeyCode.J)) SwitchTarget(false);
+
         if (Focus)
         {
             _trPointsList.Clear();
@@ -63,5 +64,36 @@ public class PlayerLook : MonoBehaviour
             _trCamera.position = _trTarget;
             _trCamera.LookAt(_trPlayer.position + Vector3.up * _height * 0.5f);
         }      
+    }
+    /// <summary>
+    /// 次のターゲットを探す
+    /// </summary>
+    /// <param name="right"></param>
+    void SwitchTarget(bool right)
+    {
+        if (_points.Count <= 1)return;
+
+        _bestTarget = null;
+        _bestAngle = 999f;
+
+        for (int i = 0; i < _points.Count; i++)
+        {
+            if (i == _targetIndex) continue;
+
+            _candidate = _points[i].transform;
+            _toCandidate = (_candidate.position - _trCamera.position).normalized;
+            //内積で判定
+            _dot = Vector3.Dot(_trCamera.right, _toCandidate);
+            if(right && _dot <= 0) continue;
+            if(!right && _dot >= 0) continue;
+
+            _angle = Vector3.Angle(_trCamera.forward, _toCandidate);
+            if(_angle < _bestAngle)
+            {
+                _bestAngle = _angle;
+                _bestTarget = _candidate;
+            }
+        }
+        if (_bestTarget != null) _targetIndex = _points.IndexOf(_bestTarget.gameObject);
     }
 }
