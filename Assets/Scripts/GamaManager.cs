c0c409aa9ebb3ded474e7e06f1381ee346b9f28d
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-09-15 02:33:29 +0900

    [add]魔法を当てたらエリア出現

--- Assets/Scripts/GamaManager.cs
+++ Assets/Scripts/GamaManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GamaManager : MonoBehaviour
 {
     public static GamaManager Instance;
+    public GameObject[] Points;
+    public Material Material;
+    Color color = Color.blue;
     private void Awake()
     {
         Application.targetFrameRate = 120;
@@ -15,6 +18,20 @@ public class GamaManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+    public void DrawTriangleArea()
+    {
+        GameObject triangle = new GameObject("Triangle");
+        MeshFilter mf = triangle.AddComponent<MeshFilter>();
+        MeshRenderer mr = triangle.AddComponent<MeshRenderer>();
+        Mesh mesh = new Mesh();
+        mesh.vertices = new Vector3[] { Points[0].transform.position, Points[1].transform.position, Points[2].transform.position };
+        mesh.triangles = new int[] { 0,1,2};
+        mesh.RecalculateNormals();
+        mf.mesh = mesh;
+        Material mat = Material;
+        mat.color = color;
+        mr.material = mat;
     }
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Magic/Bullet.cs
+++ Assets/Scripts/Magic/Bullet.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,7 +5,6 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _lifeTime = 10f;
     private Transform _tr;
     private float _speed, _power,_timer;
-    private bool _collision = false;
     public void Init(float speed,int power)
     {
         _speed = speed;
@@ -16,6 +13,7 @@ public class Bullet : MonoBehaviour
     private void Start()
     {
         _tr = GetComponent<Transform>();
+        
     }
     // Update is called once per frame
     void Update()
@@ -23,6 +21,10 @@ public class Bullet : MonoBehaviour
         _timer += Time.deltaTime;
         _tr.position += _tr.forward * _speed * Time.deltaTime;
         if (_timer > _lifeTime) Destroy(gameObject);
-        if(Physics.CheckSphere(_tr.position, 0.5f, LayerMask.GetMask("Default"))) Destroy(gameObject);
+        if (Physics.CheckSphere(_tr.position, 0.5f, LayerMask.GetMask("Point")))
+        {
+            GamaManager.Instance.DrawTriangleArea();
+        }
+        if (Physics.CheckSphere(_tr.position, 0.5f, LayerMask.GetMask("Default"))) Destroy(gameObject);
     }
 }
