0629641af6262a964ba15628d468b94f39f79e60
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-09-15 00:13:19 +0900

    [update]何も指定していないときに近くのポイントを自動で見る

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -14,7 +14,7 @@ public class PlayerLook : MonoBehaviour
     private Vector3 _trTarget,_direction,_toCandidate;
     private Transform _trPlayer, _trCamera,_bestTarget,_candidate;
     private Quaternion _rotation;
-    private float _yRotate,_baseYAngle,_currentYAngle,_bestAngle,_dot,_angle;
+    private float _bestAngle,_dot,_angle,_distanceToTarget,_bestDistance;
     public bool Focus = false;
     private int _targetIndex;
     // Start is called before the first frame update
@@ -24,48 +24,59 @@ public class PlayerLook : MonoBehaviour
         _trPlayer = GetComponent<Transform>();
         _trCamera = Camera.main.transform;
     }
-
-    // Update is called once per frame
-    void LateUpdate()
+    private void Update()
     {
-        //あとでPlayerかGameManagerへ
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K)) Focus = false;
+
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            Focus = !Focus;
-            if (Focus == false)
-            {
-                _baseYAngle = _trCamera.eulerAngles.y;
-                _yRotate = 0;
-            }
+            SwitchTarget(true);
+            Focus = true;
         }
-        if (Input.GetKeyDown(KeyCode.L)) SwitchTarget(true);
-        if (Input.GetKeyDown(KeyCode.J)) SwitchTarget(false);
-        
-        if (Focus)
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            _trPointsList.Clear();
-            foreach (GameObject obj in _points)
-            {
-                _trPointsList.Add(obj.transform.position);
-            }
-            _direction = _trPointsList[_targetIndex] - _trCamera.position;
-            _rotation = Quaternion.LookRotation(_direction);
-            _trTarget = _trPlayer.position + _rotation * new Vector3(0f, _height*0.7f, -_distance);
-            _trCamera.position = Vector3.Slerp(_trCamera.position,_trTarget,Time.deltaTime * _cameraSpeed);
-            _trCamera.rotation = Quaternion.Lerp(_trCamera.rotation, _rotation, Time.deltaTime * _cameraSpeed);
-            _yRotate = 0;
-            _mazzle.rotation = Quaternion.LookRotation(_trPointsList[_targetIndex] - _mazzle.position);
+            SwitchTarget(false);
+            Focus = true;
         }
-        else
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!Focus)
         {
-            if (Input.GetKey(KeyCode.L)) _yRotate++;
-            if (Input.GetKey(KeyCode.J)) _yRotate--;
-            _currentYAngle = _baseYAngle + _yRotate;
-            _rotation = Quaternion.Euler(0,_currentYAngle,0);
-            _trTarget = _trPlayer.position + _rotation * new Vector3(0f, _height, -_distance);
-            _trCamera.position = _trTarget;
-            _trCamera.LookAt(_trPlayer.position + Vector3.up * _height * 0.5f);
-        }      
+            _bestTarget = null;
+            _bestDistance = 100f;
+            for(int i = 0;i < _trPointsList.Count; i++)
+            {
+                _candidate = _points[i].transform;
+                _distanceToTarget = Vector3.Distance(_candidate.position,_trPlayer.position);
+                if (_distanceToTarget <_bestDistance)
+                {
+                    _bestDistance = _distanceToTarget;
+                    _bestTarget = _candidate;
+                }
+            }
+            if (_bestTarget != null) _targetIndex = _points.IndexOf(_bestTarget.gameObject);
+        }
+        FollowTarget();
+    }
+    /// <summary>
+    /// ターゲットを見つめる
+    /// </summary>
+    void FollowTarget()
+    {
+        _trPointsList.Clear();
+        foreach (GameObject obj in _points)
+        {
+            _trPointsList.Add(obj.transform.position);
+        }
+        _direction = _trPointsList[_targetIndex] - _trCamera.position;
+        _rotation = Quaternion.LookRotation(_direction);
+        _trTarget = _trPlayer.position + _rotation * new Vector3(0f, _height * 0.7f, -_distance);
+        _trCamera.position = Vector3.Slerp(_trCamera.position, _trTarget, Time.deltaTime * _cameraSpeed);
+        _trCamera.rotation = Quaternion.Lerp(_trCamera.rotation, _rotation, Time.deltaTime * _cameraSpeed);
+        _mazzle.rotation = Quaternion.LookRotation(_trPointsList[_targetIndex] - _mazzle.position);
     }
     /// <summary>
     /// 次のターゲットを探す
